7959ce0b94dc3eaaf9230ba3f56189bf0c36b28f
Carmela Manalo <MCBManalo@addu.edu.ph>
2018-12-11 08:54:39 +0800

    Changes I made during lab

--- OOP_Project/ViewModelLocator.cs
+++ OOP_Project/ViewModelLocator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OOP_Project.ViewModels;
 
 namespace OOP_Project
 {
@@ -10,6 +11,9 @@ namespace OOP_Project
     {
         public ViewModelLocator()
         {
+            MainVm = new MainVM();
         }
+
+        public MainVM MainVm { get; set; }
     }
 }
---
+++ OOP_Project/ViewModels/MainVM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace OOP_Project.ViewModels
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        private string _test;
+        
+        public string Test
+        {
+            get => _test;
+            set
+            {
+                _test = value;
+                OnPropertyChanged(Test);
+            }
+        }
+
+        public ICommand ClickCommand => new RelayCommand(Write);
+
+        public void Write()
+        {
+            Test = "Hello this is a test";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
